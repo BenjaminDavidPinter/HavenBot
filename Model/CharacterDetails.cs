e193277d47f9e883a3354e973caec9d75395dd53
Benjamin Pinter <ben.david.pinter@icloud.com>
2022-10-04 14:22:45 -0400

    CharacterDetails.cs: Added equipment model

--- Model/CharacterDetails.cs
+++ Model/CharacterDetails.cs
@@ -1,3 +1,5 @@
+using System.Text.Json.Serialization;
+
 namespace XIV.Models
 {
 
@@ -23,7 +25,104 @@ namespace XIV.Models
 	public string? DC {get;set;}
 
 	public string? FreeCompanyId {get;set;}
-	public string? FreeCompanyName {get;set;} 
+	public string? FreeCompanyName {get;set;}
+	
+	public GearSet? GearSet {get;set;}
+    }
+
+    public record class GearSet 
+    {	
+	public Attributes? Attributes {get;set;}
+	public Gear Gear {get;set;} 
+    }
+
+    public record class Gear 
+    {
+	public GearItem Body {get;set;}
+	public GearItem Bracelets {get;set;}
+	public GearItem Earrings {get;set;}
+	public GearItem Feet {get;set;}
+	public GearItem Hands {get;set;}
+	public GearItem Head {get;set;}
+	public GearItem Legs {get;set;}
+	public GearItem MainHand {get;set;}
+	public GearItem Necklace {get;set;}
+	public GearItem Ring1 {get;set;}
+	public GearItem Ring2 {get;set;}
+	public GearItem SoulCrystal {get;set;} 
+    }
+
+    public record class GearItem
+    {
+	public string? Creator {get;set;}
+	public string? Dye {get;set;}
+	public int? ID {get;set;}
+	//TODO: Implement materia object (see Reference.json)
+	public Object? Materia {get;set;}
+	public int? Mirage {get;set;}
+    }
+
+    //TODO: Someone needs to figure out what attribute maps to which label in XIV
+    public record class Attributes 
+    {
+
+	[JsonPropertyName("1")]
+	public int? Unknown_Attr1 {get;set;}
+ 
+	[JsonPropertyName("2")]
+	public int? Unknown_Attr2 {get;set;}
+ 
+	[JsonPropertyName("3")]
+	public int? Unknown_Attr3 {get;set;}
+ 
+	[JsonPropertyName("4")]
+	public int? Unknown_Attr4 {get;set;}
+ 
+	[JsonPropertyName("5")]
+	public int? Unknown_Attr5 {get;set;}
+ 
+	[JsonPropertyName("6")]
+	public int? Unknown_Attr6 {get;set;}
+ 
+	[JsonPropertyName("7")]
+	public int? Unknown_Attr7 {get;set;}
+ 
+	[JsonPropertyName("8")]
+	public int? Unknown_Attr8 {get;set;}
+ 
+	[JsonPropertyName("19")]
+	public int? Unknown_Attr19 {get;set;}
+ 
+	[JsonPropertyName("20")]
+	public int? Unknown_Attr20 {get;set;}
+ 
+	[JsonPropertyName("21")]
+	public int? Unknown_Attr21 {get;set;}
+ 
+	[JsonPropertyName("22")]
+	public int? Unknown_Attr22 {get;set;}
+ 
+	[JsonPropertyName("24")]
+	public int? Unknown_Attr24 {get;set;}
+ 
+	[JsonPropertyName("27")]
+	public int? Unknown_Attr27 {get;set;}
+ 
+	[JsonPropertyName("33")]
+	public int? Unknown_Attr33 {get;set;}
+ 
+	[JsonPropertyName("34")]
+	public int? Unknown_Attr34 {get;set;}
+ 
+	[JsonPropertyName("44")]
+	public int? Unknown_Attr44 {get;set;}
+ 
+	[JsonPropertyName("45")]
+	public int? Unknown_Attr45 {get;set;}
+ 
+	[JsonPropertyName("46")]
+	public int? Unknown_Attr46 {get;set;}
+    	
     }
 
     public record class ClassJob
